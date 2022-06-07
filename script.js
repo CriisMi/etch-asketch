e7bc094eaaea3e73509bb69744c6a6379930b320
Cris <cristinamihalciuc@yahoo.com>
2022-06-07 20:27:11 +0300

    Set up hover effect for grid cells

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const container = document.querySelector('.container');
 
 for (i = 0; i < 16; i++) {
+
+    //create a row
     const row = document.createElement('div');
     row.className = 'row';
     for (j = 0; j < 16; j++) {
+
+        //create the cells inside the row
         const cell = document.createElement('div');
         cell.className = 'cell';
         row.appendChild(cell);
         cell.innerText = (i + 1);
+
+        //add hover efect 
+        cell.addEventListener('mouseover', () => {
+            cell.className += ' hover';
+        });
+        cell.addEventListener('mouseout', () => {
+            cell.className = 'cell';
+        });
     }
-    
     container.appendChild(row);
-    
 }
+
